1a5cd9e8fc7287f0037fc78533127643b30bafa1
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-07-12 23:10:42 -0300

    feat: create not-found component

---
+++ view/src/shared/components/not-found/not-found.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center">
+        <div class="not-found-text">
+            <div class="number">
+                <h1>ooopss!!!!</h1>
+                <h2>NOT FOUND - 404</h2>
+            </div>
+        </div>
+    </div>
+</template>
