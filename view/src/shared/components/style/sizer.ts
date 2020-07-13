77b5253b8fc9a07e3d569394bc73d0ab7e249b9f
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-07-12 23:10:26 -0300

    feat: add global styling

---
+++ view/src/shared/components/style/sizer.ts
@@ -0,0 +1,7 @@
+export enum ESize {
+    LARGE = 'lg',
+    EXTRA_LARGE = 'xlg',
+    SMALL = 'sm',
+    EXTRA_SMALL = 'xsm',
+    NORMAL = 'normal'
+}
---
+++ view/src/shared/components/style/variation.ts
@@ -0,0 +1,12 @@
+export enum EVariation {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+    DEFAULT = 'default',
+    WARNING = 'warning',
+    DANGER = 'danger',
+    SUCCESS = 'success',
+    INFO = 'info',
+    LIGHT = 'light',
+    DARK = 'dark',
+    LINK = 'link'
+}
