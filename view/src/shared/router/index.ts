d596b7ddd8f60d9419173704048a24cdc9be981c
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-07-12 23:09:53 -0300

    feat: create router

---
+++ view/src/shared/router/index.ts
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: () => import('@/app/home/home.vue')
+    },
+    {
+        path: '/history',
+        name: 'History',
+        component: () => import('@/app/shortened-url/shortened-url.vue')
+    },
+    {
+        path: '*',
+        name: 'NotFound',
+        component: () => import('@/shared/components/not-found/not-found.vue')
+    }
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+});
+
+export default router;
