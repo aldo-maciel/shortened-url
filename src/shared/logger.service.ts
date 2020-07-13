0fd1b50151aac75dfb45f923933d1e0902e1d4be
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-07-12 23:06:08 -0300

    feat: add logger file cofig

---
+++ src/shared/logger.service.ts
@@ -0,0 +1,44 @@
+import { configure, getLogger } from 'log4js';
+
+import { properties } from '@/properties';
+
+const { log: props } = properties;
+
+configure({
+    appenders: {
+        errorLevel: {
+            type: 'dateFile',
+            daysToKeep: props.daysToKeep,
+            keepFileExt: true,
+            filename: props.path + 'error.log',
+            alwaysIncludePattern: true
+        },
+        allLevel: {
+            type: 'dateFile',
+            daysToKeep: props.daysToKeep,
+            keepFileExt: true,
+            filename: props.path + 'log.log',
+            alwaysIncludePattern: true,
+            level: props.level
+        },
+        stderr: {
+            type: 'logLevelFilter',
+            appender: 'errorLevel',
+            level: 'error'
+        },
+        stdout: {
+            type: 'stdout',
+            layout: {
+                type: 'colored'
+            },
+            level: props.level
+        }
+    },
+    categories: {
+        default: {
+            appenders: [ 'stdout', 'stderr', 'allLevel' ],
+            level: props.level
+        }
+    }
+});
+export default getLogger('shortened-server');
