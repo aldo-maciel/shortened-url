772936442407cf46f64b207f4838ba0db7d077d2
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-07-12 23:10:50 -0300

    feat: create navbar component

---
+++ view/src/shared/components/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+import AlButton from '@/shared/components/button/button.vue'
+
+@Component({
+    components: {
+        AlButton
+    }
+})
+export default class Navbar extends Vue {}
---
+++ view/src/shared/components/navbar/navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="navbar">
+        <h1 class="navbar-title">Shortened URL</h1>
+        <div class="navbar-items">
+            <router-link
+                class="navbar-link"
+                active-class="navbar-active-link"
+                to="/home"
+            >
+                <al-button variation="link" size="lg">
+                    {{ $t('navBar.generate') }}
+                </al-button>
+            </router-link>
+            <router-link
+                class="navbar-link"
+                active-class="navbar-active-link"
+                to="/history"
+            >
+                <al-button variation="link" size="lg">
+                    {{ $t('navBar.history') }}
+                </al-button>
+            </router-link>
+        </div>
+    </div>
+</template>
+<style lang="css" src="./navbar.css" />
+<script lang="ts" src="./navbar.ts" />
