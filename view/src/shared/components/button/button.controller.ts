d9c810424e716cf1f21254a3236e7e97d4a1e08b
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-07-12 23:11:03 -0300

    feat: create button component

---
+++ view/src/shared/components/button/button.controller.ts
@@ -0,0 +1,16 @@
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator'
+
+import { EVariation } from '@/shared/components/style/variation'
+import { ESize } from '@/shared/components/style/sizer'
+
+@Component
+export default class ButtonController extends Vue {
+    @Prop({ default: EVariation.DEFAULT }) variation!: EVariation
+    @Prop({ default: ESize.NORMAL }) size!: ESize
+    @Prop({ default: false }) disabled!: boolean
+
+    @Emit('click')
+    onClick(): void {
+        /*onclick event*/
+    }
+}
---
+++ view/src/shared/components/button/button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button
+        class="btn"
+        :class="`btn-${variation} btn-${size} ${disabled ? 'disabled' : ''}`"
+        :disable="disabled"
+        @click="onClick"
+    >
+        <span class="btn-text">
+            <slot></slot>
+        </span>
+    </button>
+</template>
+<style lang="css" src="./button.css" scoped />
+<script lang="ts" src="./button.controller.ts" />
